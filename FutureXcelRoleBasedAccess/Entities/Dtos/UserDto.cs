a2793f277eaa58eeb8c10e223b3f0757d82afded
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-23 23:11:35 +0500

    Create UserDto.cs

---
+++ FutureXcelRoleBasedAccess/Entities/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace FutureXcelRoleBasedAccess.Entities.Dtos
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
