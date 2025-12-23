e2fe520e38fbe27a3f026af8c3ee4cee0cc91693
Muhammad Fayaz <mfayaz21703@gmail.com>
2025-12-23 23:12:06 +0500

    Create _Host.cshtml.cs

---
+++ FutureXcelRoleBasedAccess/Pages/_Host.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FutureXcelRoleBasedAccess.Pages
+{
+    public class _HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
